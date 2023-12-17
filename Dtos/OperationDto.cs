f1bb11e15ebc88b8306a425f747b830b56868aaa
Jakub Zięba <jakubzieba7@gmail.com>
2023-12-17 12:28:01 +0100

    Initialize MyFinances.Core

---
+++ Dtos/OperationDto.cs
@@ -0,0 +1,19 @@
+﻿
+namespace MyFinances.Core.Dtos
+{
+    public class OperationDto
+    {
+        public int Id { get; set; }
+
+        public string Name { get; set; } = null!;
+
+        public string? Description { get; set; }
+
+        public decimal Value { get; set; }
+
+        public DateTime Date { get; set; }
+
+        public int CategoryId { get; set; }
+
+    }
+}
---
+++ Response/Response.cs
@@ -0,0 +1,12 @@
+﻿namespace MyFinances.Core.Response
+{
+    public class Response
+    {
+        public Response()
+        {
+            Errors = new List<Error>();
+        }
+        public bool IsSuccess => Errors is null || !Errors.Any();
+        public List<Error> Errors { get; set; }
+    }
+}
