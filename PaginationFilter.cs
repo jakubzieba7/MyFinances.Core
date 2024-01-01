1baac86d2b77b05b675084d6fadd3aa4ea598cd6
Jakub Zięba <jakubzieba7@gmail.com>
2024-01-01 14:08:57 +0100

    Added PaginationFilter class for Xamarin purposes

---
+++ PaginationFilter.cs
@@ -0,0 +1,34 @@
+﻿namespace MyFinances.Core
+{
+    public class PaginationFilter
+    {
+        const int maxPageSize = 20;
+        public int PageNumber { get; set; } = 1;
+        private int _pageSize = 10;
+
+        //Model bound complex types must not be abstract or value types and must have a parameterless constructor.
+        public PaginationFilter()
+        {
+            PageNumber = 1;
+            PageSize = 5;
+        }
+        public PaginationFilter(int pageNumber, int pageSize)
+        {
+            PageNumber = pageNumber;
+            PageSize = pageSize;
+        }
+
+        public int PageSize
+        {
+            get
+            {
+                return _pageSize;
+            }
+            set
+            {
+                _pageSize = (value > maxPageSize) ? maxPageSize : value;
+            }
+        }
+
+    }
+}
